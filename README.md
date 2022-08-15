086cafeeb835137ff96aeca3af6c6f768844a48e
Geremias Santos <Geremias99999@gmail.com>
2022-08-15 11:32:55 -0300

    Exchange color on the Page

---
+++ README.md
@@ -0,0 +1,11 @@
+
+## Sobre o Mini- Projeto
+
+O programa ele tem a funcionalidade de trocar a cor do corpo do site quando o botão de mudar a cor for selecionado. Contém duas páginas em HTML, sendo umas delas para o código simples da cor que será gerada do Background e uma página para mudar a cor do corpo do site em HexaDecimal.
+
+Observação: Foi utilizado a vídeo aula do FreeCodeCamp para a realização desse projeto.
+## Linguagens Utilizadas
+
+- HTML5
+- CSS3
+- JavaScript
---
+++ app.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+    //Função que seleciona o número roa rray, para retornar o valor do mesmo resultado na cor
+  const randomNumber = getRandomNumber();
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
